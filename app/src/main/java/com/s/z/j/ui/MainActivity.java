349c43a3f71370f503a328a68933c06069391796
Administrator <89579416@qq.com>
2016-12-05 17:44:45 +0800

    新增仿微信主页布局

--- app/src/main/java/com/s/z/j/ui/MainActivity.java
+++ app/src/main/java/com/s/z/j/ui/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.TextView;
 import com.s.z.j.R;
 import com.s.z.j.abcde.navigationdrawer.ui.NavigationdrawerActivity;
 import com.s.z.j.fenping.ui.FenPingActivity;
+import com.s.z.j.fragment.weixin.WeiXinFragmentActivity;
 import com.s.z.j.ui.apppackage.SystemAppPackageNameActivity;
 import com.s.z.j.ui.device.DeviceInfoActivity;
 import com.s.z.j.ui.dialog.DialogActivity;
@@ -201,8 +202,16 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @ViewInject(R.id.main_navigationdrawer_btn)
     private Button navigationdrawerBtn;
 
+    /**
+     * 分屏显示视频播放和网页显示
+     */
     @ViewInject(R.id.main_fenping_btn)
     private Button fenpingBtn;
+
+    @ViewInject(R.id.main_weixin_fragment)
+    private Button weixinBtn;
+
+
 
     private Bitmap picBitmap;//通过url获取的bitmap
     private String picUrl = "http://gb.cri.cn/mmsource/images/2010/09/27/eo100927986.jpg";//直接显示图片地址
@@ -236,6 +245,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         dialogBtn.setOnClickListener(this);
         navigationdrawerBtn.setOnClickListener(this);
         fenpingBtn.setOnClickListener(this);
+        weixinBtn.setOnClickListener(this);
         speedUtil = new SpeedUtil(this, speedHandler, new Timer());
     }
 
@@ -369,6 +379,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.main_fenping_btn:
                 startActivity(new Intent(context,FenPingActivity.class));
+                break;
+            case R.id.main_weixin_fragment:
+                startActivity(new Intent(context,WeiXinFragmentActivity.class));
                 break;
             default:
                 break;
