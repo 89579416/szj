0109cce0ef6d19b3a9e3a28f85b84bb9ecf9916c
biickvtkd <89579416@qq.com>
2017-10-30 22:57:50 +0800

    去掉了exoPlayer

--- app/src/main/java/com/s/z/j/SzjApplication.java
+++ app/src/main/java/com/s/z/j/SzjApplication.java
@@ -2,12 +2,6 @@ package com.s.z.j;
 
 import android.app.Application;
 
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
-import com.google.android.exoplayer2.util.Util;
 import com.s.z.j.logs.CrashHandler;
 import com.s.z.j.logs.DeviceManager;
 import com.s.z.j.logs.LogManager;
@@ -22,8 +16,6 @@ import org.xutils.x;
  */
 public class SzjApplication extends Application {
 
-    protected String userAgent;
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -31,20 +23,7 @@ public class SzjApplication extends Application {
         x.Ext.setDebug(BuildConfig.DEBUG); // 开启debug会影响性能
 
         HttpClient.getInstance().init(this);
-        userAgent = Util.getUserAgent(this, "小工具");
         initLogManager();
-
-    }
-    public DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
-        return new DefaultDataSourceFactory(this, bandwidthMeter,
-                buildHttpDataSourceFactory(bandwidthMeter));
-    }
-
-    public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
-        return new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter);
-    }
-    public boolean useExtensionRenderers() {
-        return BuildConfig.FLAVOR.equals("withExtensions");
     }
 
     /**
